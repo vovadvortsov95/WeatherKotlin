c4e47c0d7e225c33fa333e32bc2f087effa3e77a
vovadvortsov95 <vova.dvortsov95@gmail.com>
2018-11-13 16:00:57 +0600

    add Picasso for weather URL . Rework editText city

--- app/src/main/java/com/example/vladimirdvortsov/weatherkotlin/ui/MainActivity.kt
+++ app/src/main/java/com/example/vladimirdvortsov/weatherkotlin/ui/MainActivity.kt
@@ -1,36 +1,47 @@
 package com.example.vladimirdvortsov.weatherkotlin.ui
 
+import android.annotation.SuppressLint
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.content.Context
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
-import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import com.example.vladimirdvortsov.weatherkotlin.R
 import com.example.vladimirdvortsov.weatherkotlin.model.Weather
+import com.squareup.picasso.Picasso
 
 
+@SuppressLint("WrongViewCast")
 class MainActivity : AppCompatActivity() {
 
     // TODO : LiveData with Weather .
     // TODO : update view with bind . viewModel.bind(weatherLD)
     // mutable live data в ней сделать подписку на апи .  А при обновлении лайв даты апдейтить вью
 
-    private val viewModel by lazy { ViewModelProviders.of(this).get(WeatherView::class.java)}
+    private val viewModel by lazy { ViewModelProviders.of(this).get(WeatherView::class.java) }
 
     private lateinit var city: TextView
-    private lateinit var cityEdit : TextView
-    private lateinit var humidity: TextView
-    private lateinit var wind: TextView
-    private lateinit var pressure: TextView
-    private lateinit var temp: TextView
-    private lateinit var weatherType: TextView
-    private lateinit var locationImage : ImageView
 
-    private lateinit var context : Context
+    private val cityEdit: TextView by lazy { return@lazy findViewById<TextView>(R.id.change_city) }
+    private val humidity: TextView by lazy { return@lazy findViewById<TextView>(R.id.humidity_percent)}
+    private val wind: TextView by lazy { return@lazy findViewById<TextView>(R.id.wind_value)}
+    private val pressure: TextView by lazy {return@lazy findViewById<TextView>(R.id.preccure_value)}
+    private val temp: TextView by lazy { return@lazy findViewById<TextView>(R.id.temp) }
+    private val weatherType: TextView by lazy { return@lazy findViewById<TextView>(R.id.weather_type)}
+    private val locationImage: ImageView by lazy { return@lazy findViewById<ImageView>(R.id.geolocation) }
+
+
+    private lateinit var weatherIcon:ImageView
+
+    private lateinit var context: Context
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,12 +53,11 @@ class MainActivity : AppCompatActivity() {
         initView(context)
         //viewModel.getWeatherByCity("Omsk")
         //viewModel.getWeatherByCoord(context)
-        viewModel.weatherLD.observe( this, Observer<Weather>{
+        viewModel.weatherLD.observe(this, Observer<Weather> {
             if (it != null) {
-                Log.d("it "," != null")
+                Log.d("it ", " != null")
                 bindView(it)
-            }
-            else {
+            } else {
                 Log.d("it", "= null")
             }
 
@@ -64,24 +74,81 @@ class MainActivity : AppCompatActivity() {
         pressure.text = weather.pressure.toString()
         temp.text = weather.temp.toString()
         weatherType.text = weather.main
+ //       weatherIcon.setImageURI(Uri.parse("http://openweathermap.org/img/w/" + weather.icon + ".png"))
+
+        val iconURL = "http://openweathermap.org/img/w/" + weather.icon + ".png"
+        Picasso.get().load(iconURL).resize(250,250).into(weatherIcon)
     }
 
     private fun initView(context: Context) {
-        locationImage = findViewById(R.id.geolocation)
+        weatherIcon = findViewById(R.id.weather_icon)
+
         city = findViewById(R.id.city)
-        cityEdit = findViewById(R.id.change_city)
-        humidity = findViewById(R.id.humidity_percent)
-        wind = findViewById(R.id.wind_value)
-        pressure = findViewById(R.id.preccure_value)
-        temp = findViewById(R.id.temp)
-        weatherType = findViewById(R.id.weather_type)
-        city.isFocusable = false
-        city.isClickable = true
-//        cityEdit.setOnClickListener{
-//            city.edita
+
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        fun showKeyboard() {
+            imm.toggleSoftInput(0, 0)
+
+        }
+        city.isFocusable = false // onClick isFocusable = true // TODO : give focus for editText
+
+
+        cityEdit.setOnClickListener {
+            city.isFocusableInTouchMode = true
+            city.requestFocus()
+            Log.d("FOCUS : ", city.hasFocus().toString())
+            showKeyboard()
+        }
+
+        city.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                //never contains!
+                if (s != null)
+                    if (s.contains("\n")) {
+                        viewModel.getWeatherByCity(s.toString())
+                    }
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                Toast.makeText(this@MainActivity, "onTextChanged", Toast.LENGTH_SHORT).show()
+                Log.d("CharSequence", s.toString())
+                //never contains!
+                if (s != null)
+                    if (s.contains("\n")) {
+
+                        Log.d("FOCUS CHANGED","true")
+                        city.isFocusableInTouchMode = false
+                        city.clearFocus()
+
+                    }
+
+            }
+
+        })
+
+//        city.setOnKeyListener(View.OnKeyListener{
+//
+//                fun onKey(v :View, keyCode : Int, event : KeyEvent) : Boolean{
+//
+//                    return false;
+//
+//            }
+//
 //        }
+//
+//        );
+//        private class CustomTextWather : TextWa
+
+
         locationImage.setOnClickListener {
             viewModel.getWeatherByCoord(context)
         }
     }
 }
+
+
