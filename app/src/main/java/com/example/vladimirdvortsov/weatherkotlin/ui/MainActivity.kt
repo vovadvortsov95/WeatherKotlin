0c9c964d5c05f1217f88b6b7895cafba43228fe3
vovadvortsov95 <vova.dvortsov95@gmail.com>
2018-11-15 19:58:13 +0600

    refactoring

--- app/src/main/java/com/example/vladimirdvortsov/weatherkotlin/ui/MainActivity.kt
+++ app/src/main/java/com/example/vladimirdvortsov/weatherkotlin/ui/MainActivity.kt
@@ -8,7 +8,6 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.view.inputmethod.InputMethodManager
 import android.widget.ImageView
 import android.widget.TextView
@@ -16,36 +15,20 @@ import com.example.vladimirdvortsov.weatherkotlin.Constant
 import com.example.vladimirdvortsov.weatherkotlin.R
 import com.example.vladimirdvortsov.weatherkotlin.model.Weather
 import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 @SuppressLint("WrongViewCast")
 class MainActivity : AppCompatActivity() {
 
     private val viewModel by lazy { ViewModelProviders.of(this).get(WeatherViewModel::class.java) }
-
     private val city: TextView by lazy { return@lazy findViewById<TextView>(R.id.city) }
-
-    private val cityEdit: TextView by lazy {  findViewById<TextView>(R.id.change_city) }
-    private val humidity: TextView by lazy { return@lazy findViewById<TextView>(R.id.humidity_value) }
-    private val wind: TextView by lazy { return@lazy findViewById<TextView>(R.id.wind_value) }
-    private val pressure: TextView by lazy { return@lazy findViewById<TextView>(R.id.preccure_value) }
-    private val temp: TextView by lazy { return@lazy findViewById<TextView>(R.id.temp) }
-    private val weatherType: TextView by lazy { return@lazy findViewById<TextView>(R.id.weather_type) }
-    private val locationImage: ImageView by lazy { return@lazy findViewById<ImageView>(R.id.geolocation) }
-    private val celsia : TextView by lazy { return@lazy findViewById<TextView>(R.id.celsia)}
-    private val fahrenheit : TextView by lazy { return@lazy findViewById<TextView>(R.id.fahrenheit)}
-    private val tempType : TextView by lazy { return@lazy findViewById<TextView>(R.id.metric_type) }
     private var isCelsia : Boolean = true
-
-    private val weatherIcon: ImageView by lazy { return@lazy findViewById<ImageView>(R.id.weather_icon) }
-
-    private lateinit var context: Context
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        context = applicationContext
-        initView(context)
+        initView(applicationContext)
     }
 
     private fun bindView(weather: Weather?) {
@@ -54,22 +37,22 @@ class MainActivity : AppCompatActivity() {
             city.text = weather.city
             humidity.text = weather.humidity.toString()
             wind.text = windInfo
-            pressure.text = weather.pressure.toString()
-            weatherType.text = weather.main
+            preccure.text = weather.pressure.toString()
+            weather_type.text = weather.main
             setWeatherType(weather)
 
-            Picasso.get().load(Constant.imageUrl + weather.icon + ".png").resize(250, 250).into(weatherIcon)
+            Picasso.get().load(Constant.imageUrl + weather.icon + ".png").resize(250, 250).into(weather_icon)
         }
     }
 
     private fun setWeatherType(weather: Weather) {
         if (isCelsia) {
             val newTemp = (weather.temp - 273.15).toString()
-            tempType.text = " °C"
+            metric_type.text = " °C"
             temp.text = newTemp
         } else {
             val newTemp = weather.temp.toString()
-            tempType.text = " °F"
+            metric_type.text = " °F"
             temp.text = newTemp
         }
     }
@@ -88,7 +71,7 @@ class MainActivity : AppCompatActivity() {
         }
         city.isFocusable = false
 
-        cityEdit.setOnClickListener {
+        change_city.setOnClickListener {
             city.isFocusableInTouchMode = true
             city.requestFocus()
             showKeyboard()
@@ -98,7 +81,7 @@ class MainActivity : AppCompatActivity() {
             if (!temp.text.isNullOrEmpty()){
                 if (isCelsia) {
                     val newTemp = (temp.text.toString().toDouble() - 273.15).toString()
-                    tempType.text = " °F"
+                    metric_type.text = " °F"
                     temp.text = newTemp
                     isCelsia = false
                     celsia.setBackgroundColor(this.resources.getColor(R.color.colorGrey))
@@ -106,7 +89,7 @@ class MainActivity : AppCompatActivity() {
                 }
                 else{
                     val newTemp = (temp.text.toString().toDouble() + 273.15).toString()
-                    tempType.text = " °C"
+                    metric_type.text = " °C"
                     temp.text = newTemp
                     isCelsia = true
                     celsia.setBackgroundColor(this.resources.getColor(R.color.cardBackground))
@@ -120,14 +103,14 @@ class MainActivity : AppCompatActivity() {
             if (!temp.text.isNullOrEmpty()){
                 if (isCelsia) {
                     temp.text = (temp.text.toString().toDouble() - 273.15).toString()
-                    tempType.text = " °F"
+                    metric_type.text = " °F"
                     isCelsia = false
                     celsia.setBackgroundColor(this.resources.getColor(R.color.colorGrey))
                     fahrenheit.setBackgroundColor(this.resources.getColor(R.color.cardBackground))
                 }
                 else{
                     temp.text = (temp.text.toString().toDouble() + 273.15).toString()
-                    tempType.text = " °C"
+                    metric_type.text = " °C"
                     isCelsia = true
                     celsia.setBackgroundColor(this.resources.getColor(R.color.cardBackground))
                     fahrenheit.setBackgroundColor(this.resources.getColor(R.color.colorGrey))
@@ -154,7 +137,7 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
-        locationImage.setOnClickListener {
+        geolocation.setOnClickListener {
             viewModel.getWeatherByCoord(context)
         }
     }
